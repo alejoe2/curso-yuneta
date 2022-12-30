5414b5178b131732c0f6b78ed1f78dcb3408e5b7
alejoe2 <echeverrialuish@hotmail.com>
2022-12-30 17:27:36 +0100

    hello world create

---
+++ 01-print-hello-world/c/build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/luis_e/projects/curso-yuneta/01-print-hello-world/c/main.c" "CMakeFiles/HelloWorld.dir/main.c.o" "gcc" "CMakeFiles/HelloWorld.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
