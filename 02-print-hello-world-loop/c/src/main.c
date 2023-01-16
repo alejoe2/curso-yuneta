b2815679da8a1871724cc1e603445042f0868553
alejo2e <echeverrialuish@hotmail.com>
2023-01-16 12:55:38 +0100

    fix cal diff time

--- 02-print-hello-world-loop/c/src/main.c
+++ 02-print-hello-world-loop/c/src/main.c
@@ -1,15 +1,32 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> // clock_t, clock(), CLOCKS_PER_SEC
 
+/***************************************************************************
+ *                      Calcule diff time
+ ***************************************************************************/
+static inline struct timespec ts_diff(struct timespec start, struct timespec end)
+{
+    struct timespec temp;
+    if((end.tv_nsec - start.tv_nsec) < 0) {
+        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return temp;
+}
+
+/***************************************************************************
+ *                      Main
+ ***************************************************************************/
 int main(int argc, char *argv[])
 {
-    clock_t start_t, end_t;
-    double total_t;
+    struct timespec start_t, end_t, diff_t;
     int maxLoop = 100;
 
-    start_t = clock();
+    clock_gettime(CLOCK_MONOTONIC, &start_t);
 
     if(argc == 2) {
         maxLoop = atoi(argv[1]);
@@ -19,8 +36,9 @@ int main(int argc, char *argv[])
         printf("Hello World: %d \n", i);
     }
 
-    end_t = clock();
-    total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-    printf("Total time taken by CPU: %f\n", total_t);
+    clock_gettime(CLOCK_MONOTONIC, &end_t);
+
+    diff_t = ts_diff(start_t, end_t);
+    printf("Total time taken by CPU: %lu.%lus\n", diff_t.tv_sec, diff_t.tv_nsec);
     return 0;
 }
