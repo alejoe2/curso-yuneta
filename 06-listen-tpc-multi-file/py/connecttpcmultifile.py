6eb5c086b573edf286cfcebe505a5671889a4016
luis <echeverrialuish@hotmail.com>
2023-02-01 18:22:15 +0100

    fix list files

--- 06-listen-tpc-multi-file/py/connecttpcmultifile.py
+++ 06-listen-tpc-multi-file/py/connecttpcmultifile.py
@@ -22,7 +22,7 @@ def main():
     group1 = parser.add_argument_group('options for execution')
     group1.add_argument("-p", "--port", help="define port to use, default port=7000", type=int, default=port)
     group1.add_argument("-i", "--ip", help="define ip to use, default ip='127.0.0.1'", type=str, default=ip)
-    group1.add_argument("FILES", help="FILES to send, defaul=listfiles.txt ", type=str)
+    group1.add_argument("FILES", nargs="+", help="list FILES to send ", type=str)
     args = parser.parse_args()
 
     if args.port:
@@ -42,15 +42,8 @@ def connectport(port, ip, FILES):
 
     # Connect to the server
     client_socket.connect((ip, port))
-
-    # open list files to be sent
-    files = open(FILES, "rb")
-
-    listFile=files.read().decode().split("\n")
-
-    files.close()
     
-    for i, filepath in enumerate(listFile):
+    for i, filepath in enumerate(FILES):
         
         if filepath !=  "":
             print("Send: ", filepath)
@@ -61,10 +54,10 @@ def connectport(port, ip, FILES):
                     file_data = (filepath+'\0').encode() + f.read()
                     client_socket.sendall(file_data)
                 # receive confirmation
-                if(i < len(listFile)-1):
+                if(i < len(FILES)):
                     confirmation = client_socket.recv(1024)
                     print(f'Received: {confirmation.decode()}')
-            
+
             except FileNotFoundError:
                 print("The file is not found\n")
     
